e157217da104991fb22431fd075d272118950000
RobDab <roberto.dabb@gmail.com>
2022-09-26 17:12:37 +0200

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+let date = new Date()
+console.log(date)
+
+console.log(date.toLocaleDateString('it-IT'))
+
+console.log(date.getDate())
+console.log(date.getMonth() + 1)
+console.log(date.getFullYear())
+
+function showDate(){
+    document.getElementById("day").innerHTML = date.getDate()
+    document.getElementById("month").innerHTML = date.getMonth() + 1
+    document.getElementById("year").innerHTML = date.getFullYear()
+}
