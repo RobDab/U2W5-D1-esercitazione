d5071ec671db1d8546346092f65d82fe74282b1c
RobDab <roberto.dabb@gmail.com>
2022-09-26 17:24:50 +0200

    style and current dateTime implemented

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ console.log(date.getDate())
 console.log(date.getMonth() + 1)
 console.log(date.getFullYear())
 
+document.getElementById('firstDate').innerHTML = date
+
 function showDate(){
     document.getElementById("day").innerHTML = date.getDate()
     document.getElementById("month").innerHTML = date.getMonth() + 1
